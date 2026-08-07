f1a4cd4ce3a62512fb30d7277bf8bf9dfbbb3a38
Redish03 <pluto0303@pukyong.ac.kr>
2026-08-07 11:12:11 +0900

    feat: 여행기록을 수정할 수 있다

--- client/shared/src/commonMain/kotlin/com/mapmory/shared/domain/TripRecords.kt
+++ client/shared/src/commonMain/kotlin/com/mapmory/shared/domain/TripRecords.kt
@@ -1,5 +1,6 @@
 package com.mapmory.shared.domain
 
+import kotlinx.datetime.LocalDate
 import kotlin.uuid.Uuid
 
 class TripRecords(
@@ -7,7 +8,7 @@ class TripRecords(
 ) {
     private val records: List<TripRecord> = tripRecords.toList()
 
-    val _tripRecords: List<TripRecord>
+    val tripRecords: List<TripRecord>
         get() = records.toList()
 
     fun addTripRecord(
@@ -15,12 +16,16 @@ class TripRecords(
         tripRecordTitle: String,
         tripRecordDescription: String?,
         tripLocation: String,
+        startTripDate: LocalDate,
+        endTripDate: LocalDate,
     ): TripRecords {
         val newRecord = TripRecord(
             imageUrl = imageUri,
             tripRecordTitle = tripRecordTitle,
             tripRecordDescription = tripRecordDescription,
-            location = tripLocation
+            location = tripLocation,
+            startTripDate = startTripDate,
+            endTripDate = endTripDate,
         )
         return TripRecords(records + newRecord)
     }
@@ -28,12 +33,35 @@ class TripRecords(
     fun removeTripRecord(
         deletingRecord: TripRecord
     ): TripRecords {
-        val record = records.find { it.id == deletingRecord.id }
-        if(record == null) return this
+        val record = findTripRecordId(deletingRecord.id) ?: return this
 
         return TripRecords(records.minus(record))
     }
 
-    private fun findTripRecordId(id: Uuid): Uuid? = records.find { it.id == id }?.id
+    fun editTripRecord(
+        editingRecord: TripRecord,
+        editingImage: String?,
+        editingTitle: String?,
+        editingDescription: String?,
+        editingLocation: String?,
+    ): TripRecords {
+        val record = findTripRecordId(editingRecord.id)
+            ?: throw IllegalArgumentException("해당 id를 찾을 수 없습니다")
+
+        val editedRecord = record.copy(
+            imageUrl = editingImage ?: record.imageUrl,
+            tripRecordTitle = editingTitle ?: record.tripRecordTitle,
+            tripRecordDescription = editingDescription ?: record.tripRecordDescription,
+            location = editingLocation ?: record.location,
+        )
+
+        return TripRecords(
+            records.map { currentRecord ->
+                if (currentRecord.id == record.id) editedRecord else currentRecord
+            },
+        )
+    }
+
+    private fun findTripRecordId(id: Uuid): TripRecord? = records.find { it.id == id }
 
 }
