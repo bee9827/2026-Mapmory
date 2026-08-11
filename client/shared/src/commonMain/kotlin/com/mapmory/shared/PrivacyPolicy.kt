b7c280a44ec3794cc750a8b6b1f12a54efbccb42
Redish03 <pluto0303@pukyong.ac.kr>
2026-08-11 15:18:09 +0900

    feat: add privacy policy link placeholder

---
+++ client/shared/src/commonMain/kotlin/com/mapmory/shared/PrivacyPolicy.kt
@@ -0,0 +1,11 @@
+package com.mapmory.shared
+
+/**
+ * Privacy policy link configuration.
+ *
+ * Set [URL] when the privacy policy page is published. The profile screen
+ * displays the link only when this value is not blank.
+ */
+object PrivacyPolicy {
+    const val URL = ""
+}
