e4684abc96224d57480801f7b73c46d134c1bfa3
Redish03 <pluto0303@pukyong.ac.kr>
2026-08-11 16:12:38 +0900

    build: 버전 증가

--- client/androidApp/build.gradle.kts
+++ client/androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.mapmory.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         resValue(
             type = "string",
             name = "mapmory_api_base_url",
